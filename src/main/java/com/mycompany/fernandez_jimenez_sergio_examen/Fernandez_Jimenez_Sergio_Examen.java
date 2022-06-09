ddd907dc366374d1f41ce9bb70c1d5417cf73cf9
Fernandez <Fernandez@192.168.1.81>
2022-06-09 12:40:00 +0200

    probando la maquina virtual

---
+++ src/main/java/com/mycompany/fernandez_jimenez_sergio_examen/Fernandez_Jimenez_Sergio_Examen.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.fernandez_jimenez_sergio_examen;
+
+/**
+ *
+ * @author Fernandez
+ */
+public class Fernandez_Jimenez_Sergio_Examen {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
